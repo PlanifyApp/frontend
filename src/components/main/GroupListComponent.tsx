e6f3f24419aa0f1251a239f77c76a1c4989ed65b
JeanYoungPark <jean.young.park.96@gmail.com>
2024-05-27 10:10:14 +0900

    feat: 그룹리스트 호출

--- src/components/main/GroupListComponent.tsx
+++ src/components/main/GroupListComponent.tsx
@@ -1,25 +1,51 @@
-import { List, ListItem, ListItemText } from '@mui/material';
-import { useEffect } from 'react';
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { api } from '../../apis/baseApi';
+import { CustomList, CustomListItem, CustomListItemText } from '../../assets/styles/aside.styles';
+
+type groupType = {
+    title: string;
+    color: string;
+};
 
 export const GroupListComponent = () => {
+    const [group, setGroup] = useState<groupType[]>([]);
+
     useEffect(() => {
         const getUserGroup = async () => {
-            const res = await api.get('/group/list');
-            console.log(res);
+            try {
+                const { data } = await api.get('/group/list');
+
+                if (data.status == 200) {
+                    setGroup(data.newData);
+                }
+            } catch (error) {
+                console.log(error);
+            }
         };
 
         getUserGroup();
     }, []);
 
+    console.log(group.length);
     return (
-        <List>
-            <ListItem>
-                <ListItemText primary="운동" />
-            </ListItem>
-            <ListItem>
-                <ListItemText primary="업무" />
-            </ListItem>
-        </List>
+        <CustomList>
+            {group.length > 0 &&
+                group.map((data, index) => (
+                    <CustomListItem key={index}>
+                        <ListItemIcon
+                            sx={{
+                                minWidth: 'initial',
+                                width: '20px',
+                                height: '20px',
+                                borderRadius: '50%',
+                                marginRight: '10px',
+                                backgroundColor: data.color
+                            }}
+                        ></ListItemIcon>
+                        <CustomListItemText primary={data.title} />
+                    </CustomListItem>
+                ))}
+        </CustomList>
     );
 };
